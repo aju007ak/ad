34381cbb28dd2f1046ca96340ffc2d5a0e059006
aju007ak <john.ajith95ak@gmail.com>
2019-06-07 11:54:50 +0530

    Update dv.py

--- dv.py
+++ dv.py
@@ -1,18 +1,12 @@
-def getPairsCount(arr, n, sum): 
-      
-    count = 0 # Initialize result 
-  
-
-    for i in range(0, n): 
-        for j in range(i + 1, n): 
-            if arr[i] + arr[j] == sum: 
-                count += 1
-      
-    return count 
-  
-
-arr = [1, 5, 7, -1, 5] 
-n = len(arr) 
-sum = 6
-print("Count of pairs is", 
-      getPairsCount(arr, n, sum)) 
+import sys, string, math
+n,k = input().split()
+n,k = int(n), int(k)
+L = [ int(x) for x in input().split()]
+for i in range(0,k) :
+    x = 0
+    a,b = input().split()
+    a,b = int(a), int(b)
+    for j in range(a-1,b) :
+        x = x ^ L[j]
+        #print(L[j],x)
+    print(x)
